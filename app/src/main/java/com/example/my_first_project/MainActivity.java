f9c16a93aa9429c38e814ac80fa3269ee1092eb0
Kirill Nikolaev <kirill.nikolaev.98@gmail.com>
2021-03-15 22:23:32 +0300

    add variables

--- app/src/main/java/com/example/my_first_project/MainActivity.java
+++ app/src/main/java/com/example/my_first_project/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.my_first_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    byte y = 125;
+    short a = (short) 40000;
+    int b = 50000;
+    long c = 1;
+    float d = 3.1F;
+    double e = 4.13235;
+    boolean r = true;
+    char t = 'o';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
