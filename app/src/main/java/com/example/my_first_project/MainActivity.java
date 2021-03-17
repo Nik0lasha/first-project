db787f39167e577c8ba583c40ddc0d7c32c5b99d
Nik0lasha <Kirill.nikolaev.98@gmail.com>
2021-03-17 14:33:29 +0300

    work_with_variables

--- app/src/main/java/com/example/my_first_project/MainActivity.java
+++ app/src/main/java/com/example/my_first_project/MainActivity.java
@@ -20,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("example_boolean", String.valueOf(r));
+        Log.i("example_byte", String.valueOf(y));
+        Log.i("example_short", String.valueOf(a));
+        Log.i("example_int", String.valueOf(b));
+        Log.i("example_float", String.valueOf(d));
+        Log.i("example_long", String.valueOf(c));
+        Log.i("example_double", String.valueOf(e));
+        Log.i("example_char", String.valueOf(t));
     }
 }
