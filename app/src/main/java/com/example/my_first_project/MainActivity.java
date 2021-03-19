fb837772b250a1ad08f0c82341373658d49a3930
Nik0lasha <Kirill.nikolaev.98@gmail.com>
2021-03-19 16:56:53 +0300

    task 2 emulator

--- app/src/main/java/com/example/my_first_project/MainActivity.java
+++ app/src/main/java/com/example/my_first_project/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.my_first_project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvRes;
+    private EditText eText;
+    private EditText eText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvRes = findViewById(R.id.tvresult);
+        eText = findViewById(R.id.editText);
+        eText2 = findViewById(R.id.editText2);
     }
+
+    {
+        int a = Integer.parseInt(eText.getText().toString(10));
+        int b = Integer.parseInt(eText2.getText().toString());
+        int c = Integer.parseInt(eText2.getText().toString());
+        int result= a + b;
+
+          tvRes.setText(String.valueOf(result));
+
+
+
+        System.out.println("a == b=" +(a == b));
+        System.out.println("a != b =" +(a != b));
+        System.out.println("a > b =" +(a > b));
+        System.out.println("a < b =" +(a < b));
+        System.out.println("a >= b =" +(a >= b));
+        System.out.println("a <= b =" +(a <= b));
+
+
+
+
+        c = a + b;
+        System.out.println("c = a + b  =" +c);
+
+        c += a;
+        System.out.println("c += a =" +c);
+
+        c -= a;
+        System.out.println("c -= a =" +c);
+
+        c *= a;
+        System.out.println("c *= a =" +c);
+
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a" +c);
+
+
+        int p = 5;
+        int w = 10;
+        int e = 25;
+        System.out.println("p + w=" +(p + w));
+        System.out.println("p - w =" +(p - w));
+        System.out.println("p * w =" +(p * w));
+        System.out.println("p / w =" +(p / w));
+        System.out.println("w % p =" +(w % p));
+        System.out.println("e % p =" +(e % p));
+
+        boolean v = true;
+        boolean t = false;
+        boolean q = v | t;
+        boolean d = v & t;
+
+        System.out.println(" v = " +v);
+        System.out.println(" t = " +b);
+        System.out.println(" v|t = " +q);
+        System.out.println(" v&t = " +d);
+    }
+    public void onClickResult(View view)
+    {}
+
 }
