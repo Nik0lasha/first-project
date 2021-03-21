f99ffc01242d140add4bac92a6c322f2150d01bb
Nik0lasha <Kirill.nikolaev.98@gmail.com>
2021-03-21 15:45:18 +0300

    task 2  the end home

--- app/src/main/java/com/example/my_first_project/MainActivity.java
+++ app/src/main/java/com/example/my_first_project/MainActivity.java
@@ -1,82 +1,102 @@
 package com.example.my_first_project;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvRes;
-    private EditText eText;
-    private EditText eText2;
+    int a = 10;
+    int b = 20;
+    int c = 0;
+    TextView textViewCompare;
+    TextView textViewMSum;
+    TextView textViewMin;
+    TextView textViewIncrease;
+    TextView textViewsplit;
+    TextView textViewPercent;
+    TextView textViewPercent2;
+    TextView textViewBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvRes = findViewById(R.id.tvresult);
-        eText = findViewById(R.id.editText);
-        eText2 = findViewById(R.id.editText2);
+        textViewCompare = findViewById(R.id.textViewUser);
+        textViewMSum = findViewById(R.id.textViewSum);
+        textViewMin = findViewById(R.id.textViewMin);
+        textViewIncrease = findViewById(R.id.textViewIncrease);
+        textViewsplit = findViewById(R.id.textViewSplit);
+        textViewPercent = findViewById(R.id.textViewPercent);
+        textViewPercent2 = findViewById(R.id.textViewPercent2);
+        textViewBoolean = findViewById(R.id.textViewBoolean);
+
+
+        textViewCompare.setText("Результат выражения: a == b; false " +
+                "\nРезультат выражения: a != b; true" +
+                "\nРезультат выражения: a > b; false" +
+                "\nРезультат выражения: a < b; true" +
+                "\nРезультат выражения: b >= a; true" +
+                "\nРезультат выражения: b <= a; false" +
+                "\nРезультат выражения: c = a + b; 30" +
+                "\nРезультат выражения: c += a; 10" +
+                "\nРезультат выражения: c -= a; -10" +
+                "\nРезультат выражения: c *= a; 0" +
+                "\nРезультат выражения: где c = 15; c /= a; 1    ");
+
+        int a = 5;
+        int b = 10;
+        int c = 25;
+
+        int f = a + b;
+        int f1 = a - b;
+        int f2 = a * b;
+        int f3 = b / a;
+        int f4 = b % a;
+        int f5 = c % a;
+
+        textViewMSum.append(String.valueOf(a + "+" + b + "=" + f));
+        textViewMin.append(String.valueOf(a + "-" + b + "=" + f1));
+        textViewIncrease.append(String.valueOf(a + "*" + b + "=" + f2));
+        textViewsplit.append(String.valueOf(b + "/" + a + "=" + f3));
+        textViewPercent.append(String.valueOf(b + "%" + a + "=" + f4));
+        textViewPercent2.append(String.valueOf(c + "%" + a + "=" + f5));
+
+
+
+        textViewBoolean.setText("Результат выражения: a = true + true; true " +
+                "\nРезультат выражения: b = false + false; false" +
+                "\nРезультат выражения: a | b + a | b; true" +
+                "\nРезультат выражения: a & b + a & b; false ");
+
+        int month = 3;
+        String season;
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                season = "Зимука зима";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Весна";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Лето";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Осень";
+                break;
+            default:
+                season = "Не знаю";
+        }
+        System.out.println("March is in the " +    season +     ".");
+
     }
-
-    {
-        int a = 10;
-        int b =20;
-        int c =0;
-        System.out.println("a == b=" +(a == b));
-        System.out.println("a != b =" +(a != b));
-        System.out.println("a > b =" +(a > b));
-        System.out.println("a < b =" +(a < b));
-        System.out.println("a >= b =" +(a >= b));
-        System.out.println("a <= b =" +(a <= b));
-
-
-
-
-        c = a + b;
-        System.out.println("c = a + b  =" +c);
-
-        c += a;
-        System.out.println("c += a =" +c);
-
-        c -= a;
-        System.out.println("c -= a =" +c);
-
-        c *= a;
-        System.out.println("c *= a =" +c);
-
-        a = 10;
-        c = 15;
-        c /= a;
-        System.out.println("c /= a" +c);
-
-
-        int p = 5;
-        int w = 10;
-        int e = 25;
-        System.out.println("p + w=" +(p + w));
-        System.out.println("p - w =" +(p - w));
-        System.out.println("p * w =" +(p * w));
-        System.out.println("p / w =" +(p / w));
-        System.out.println("w % p =" +(w % p));
-        System.out.println("e % p =" +(e % p));
-
-        boolean v = true;
-        boolean t = false;
-        boolean q = v | t;
-        boolean d = v & t;
-
-        System.out.println(" v = " +v);
-        System.out.println(" t = " +b);
-        System.out.println(" v|t = " +q);
-        System.out.println(" v&t = " +d);
-    }
-    public void onClickResult(View view)
-    {}
-
 }
